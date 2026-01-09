1498503f12ec45901c51049d05a42d0b27e1ed24
Artee13 <itemalook@gmail.com>
2026-01-09 19:31:28 -0330

    Connect Spring Boot to MySQL and add health endpoint

---
+++ src/main/java/com/keyin/sdat_devops_final_backend/SdatDevopsFinalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.keyin.sdat_devops_final_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdatDevopsFinalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdatDevopsFinalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keyin/sdat_devops_final_backend/SdatDevopsFinalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyin.sdat_devops_final_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdatDevopsFinalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
