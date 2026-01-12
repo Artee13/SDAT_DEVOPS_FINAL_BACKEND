9a1b3b3b12636561788310cd49d2be3ffe1b5f98
Artee13 <itemalook@gmail.com>
2026-01-12 14:49:18 -0330

    Add minimal backend tests

---
+++ src/test/java/com/keyin/sdat_devops_final_backend/SimpleMathTest.java
@@ -0,0 +1,13 @@
+package com.keyin.sdat_devops_final_backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SimpleMathTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
